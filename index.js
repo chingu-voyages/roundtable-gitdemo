bdfb1d245c2b22526abc82573d511e7867109fb2
Jim Medlock <jdmedlock@gmail.com>
2021-10-06 15:02:30 -0500

    Add comment

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const elems = {
   fahrenheit: document.querySelector("input#fahrenheit"),
 };
 
+// Convert Celcius to Fahenheit for non-metric users
 function celsiusToFahrenheit(temp) {
   return (temp * 9) / 5 + 32;
 }
