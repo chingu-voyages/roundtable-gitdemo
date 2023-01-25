d5aabb586c9f6cee60bd1840b56ad8482172514e
jdmedlock <jdmedlock@gmail.com>
2023-01-25 17:49:09 -0600

    Refactor: Update comment to add clarity around Luis's awesomeness

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const elems = {
 }
 
 // Convert Celcius to Fahenheit for non-Metric users
-// This is a comment
+// This is a really good comment created for Medic1111
 function celsiusToFahrenheit(temp) {
   console.log('celsiusToFahrenheit - temperature: ', temp)
   return (temp * tempConvOffset9) / tempConvOffset5 + zeroInCelcius
