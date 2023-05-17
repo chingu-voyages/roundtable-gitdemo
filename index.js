baf5a462947cc40460583b7a2522b153cb2b8f5e
jdmedlock <jdmedlock@gmail.com>
2023-05-17 18:06:13 -0500

    Refactor: Update comment

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const tempConvOffset5 = 5
 const tempConvOffset9 = 9
 
 // Create input fields for temperature values
+// for celcius & fahrenheit
 const elems = {
   celsius: document.querySelector("input#celsius"),
   fahrenheit: document.querySelector("input#fahrenheit"),
