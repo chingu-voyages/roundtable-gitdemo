519bb2e3a9e9be5c99a0e13deece739ecb5dc3b9
jdmedlock <jdmedlock@gmail.com>
2023-05-20 10:12:22 -0500

    Refactor: Update todo comment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const tempConvOffset5 = 5
 const tempConvOffset9 = 9
 
 // Create input fields for temperature values
-// TODO: Validate celcius & fahrenheit inputs
+// TODO: Validate the celcius & fahrenheit input field contents 
 const elems = {
   celsius: document.querySelector("input#celsius"),
   fahrenheit: document.querySelector("input#fahrenheit"),
