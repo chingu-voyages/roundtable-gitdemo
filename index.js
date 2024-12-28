05675db741b7604b42f8e58374fe514602a85338
jdmedlock <jdmedlock@gmail.com>
2024-12-28 10:05:06 -0600

    Refactor: Correct Comment
    Removed a silly comment added for demo purposes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Constants required for conversion between Celcius & Fahrenheit Andres is at the beach
+// Constants required for conversion between Celcius & Fahrenheit
 const zeroInCelcius = 32  // This is the Celcius equivalent to the freezing point
 const tempConvOffset5 = 5 // Celcius to Fahrenheit factor to be used for conversion 
 const tempConvOffset9 = 9 // Fahrenheit to Celcius conversion factor
