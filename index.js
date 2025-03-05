873533ad3a601bb22b7a6db5acdf6b9d83804794
jdmedlock <jdmedlock@gmail.com>
2025-03-05 17:23:18 -0600

    Refactor: Update comments
    Update comments to correct the misspelling of

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-// Constants required for conversion between Celcius & Fahrenheit
-const zeroInCelcius = 32  // This is the Celcius version of the freezing point
-const tempConvOffset5 = 5 // Celcius to Fahrenheit factor to be used for conversion 
-const tempConvOffset9 = 9 // Fahrenheit to Celcius conversion factor
+// Constants required for conversion between Celsius & Fahrenheit
+const zeroInCelsius = 32  // This is the Celsius version of the freezing point
+const tempConvOffset5 = 5 // C-to-F factor to be used for conversion 
+const tempConvOffset9 = 9 // Fahrenheit to Celsius conversion factor
 
-// Create input fields for numeric values in both Mister Celcius and Miss Fahrenheit. 
+// Create input fields for numeric values in both Mister Celsius and Miss Fahrenheit. 
 // This will be used in the temperature conversion logic.
 const elems = {
   celsius: document.querySelector("input#celsius"),
   fahrenheit: document.querySelector("input#fahrenheit"),
 }
 
-// Convert Celcius to Fahenheit for non-Metric users. 
+// Convert Celsius to Fahenheit for non-Metric users. 
 function celsiusToFahrenheit(temp) {
   console.log(`celsiusToFahrenheit - temperature: ${ temp }`)
-  return (temp * tempConvOffset9) / tempConvOffset5 + zeroInCelcius
+  return (temp * tempConvOffset9) / tempConvOffset5 + zeroInCelsius
 }
 
-// Convert a temperature from Fahenheit to Celcius
+// Convert a temperature from Fahenheit to Celsius
 function fahrenheitToCelsius(temp) {
-  return ((temp - zeroInCelcius) * tempConvOffset5) / tempConvOffset9
+  return ((temp - zeroInCelsius) * tempConvOffset5) / tempConvOffset9
 }
 
-// Process a change in the Celcius temperature as input by the user
+// Process a change in the Celsius temperature as input by the user
 function onCelsiusInput(event) {
   const { value } = event.target
   fahrenheit.value = celsiusToFahrenheit(value).toFixed(2)
