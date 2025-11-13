11f518eefec06757c398235bcdb027e9196a3012
Jim Medlock <jdmedlock@gmail.com>
2025-11-12 18:01:14 -0600

    Fix typo in Celsius to Fahrenheit log message

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const elems = {
 
 // Convert Celsius to Fahenheit for non-Metric users. 
 function celsiusToFahrenheit(temp) {
-  console.log(`CtoF - temperature: ${ temp }`)
+  console.log(`CelciustoF - temperature: ${ temp }`)
   return (temp * tempConvOffset9) / tempConvOffset5 + zeroInCelsius
 }
 
